a8a3005b8ffb13691b30cca86013a610601c1d03
alexslzr <alejandroslzr17@gmail.com>
2023-11-19 23:27:57 -0600

    added themes and colors

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import LineChart from '../../components/Charts/LineChart'
+import Header from '../../components/Header'
 
 const Line = () => {
   return (
-    <div>
-      
+    <div className='m-2 md:m-10 p-2 md:p-10 mt-24 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title='Inflation Rate'/>
+      <div className='w-full'>
+        <LineChart />
+      </div>
     </div>
   )
 }
